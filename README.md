19ada9a4bb7c2d4d5227dd1393b2039fae785775
Dominic Humphries <dominic@oneandoneis2.com>
2013-12-16 15:10:28 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Git::Archive - Blah blah blah
+
+# SYNOPSIS
+
+    use Git::Archive;
+
+# DESCRIPTION
+
+Git::Archive is
+
+# AUTHOR
+
+Dominic Humphries <dominic@oneandoneis2.com>
+
+# COPYRIGHT
+
+Copyright 2013- Dominic Humphries
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Git/Archive.pm
@@ -0,0 +1,39 @@
+package Git::Archive;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Git::Archive - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Git::Archive;
+
+=head1 DESCRIPTION
+
+Git::Archive is
+
+=head1 AUTHOR
+
+Dominic Humphries E<lt>dominic@oneandoneis2.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Dominic Humphries
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
