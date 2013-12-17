37b312ee414f50d63a9e074edc8e0bdced6c61c4
Dominic Humphries <dominic@oneandoneis2.com>
2013-12-17 14:44:48 +0000

    Possibly complete, probably about time to write some tests...

--- lib/Git/Archive.pm
+++ lib/Git/Archive.pm
@@ -10,105 +10,44 @@ use Data::Dumper;
 sub commit {
     my $self = shift;
     # Get passed-in arguments correctly into hash
-    my %args = ref $_[0] eq 'HASH' ? %{ shift @_ } : @_;
+    my $args = ref $_[0] eq 'HASH' ? shift @_ : {@_};
 
     # Check for mandatory args
     ## First, make sure we have an error sub defined
-    my $error = $args{error} // sub { my $error = shift; print STDERR $error; return 1; };
+    my $error =  $args->{error}
+              // sub { my ($args, $error) = @_; print STDERR "[ERROR] $error"; return 1; };
 
     ## Now throw errors if necessary
-    unless ( $args{msg} ) {
-        return $error->('No commit message supplied');
+    unless ( $args->{msg} ) {
+        return $error->( $args,'No commit message supplied');
         }
-    unless ( $args{files} || $args{all_tracked} || $args{all_dirty} ) {
-        return $error->('No files specified to commit');
+    unless ( $args->{files} || $args->{all_tracked} || $args->{all_dirty} ) {
+        return $error->( $args,'No files specified to commit');
         }
 
-    # Seems all is well. Check if the environment is sane
+    # Seems all is well with args. Check if the environment is sane
     ## Is the current or passed-in directory a git repo?
-    my $repo;
-    eval { $repo = Git::Repository->new( git_dir => $args{git_dir} ); };
-    if ($@) { return $error->($@) }
+    my $repo = $self->_get_repo( $args );
+    unless ( $repo ) { return $error->( $args, $args->{error} ) }
 
     ## Are there files already staged?
     if ( $repo->run( qw/diff --cached --name-only/ ) ) {
-        return $error->('Repo already has staged files');
+        return $error->( $args,'Repo already has staged files');
         }
 
-    # Looks like we're good to go. Stage the files
-    my $files;
-    if ( $args{files} ) {
-        ## We have a list of specified files to commit
-        $files = $self->filenames( \%args );
-        eval { $repo->run( add => $files ); };
-        ## Do we need to make sure all the files had changes to stage?
-        if ( $args{check_all_staged} ) {
-            my @staged = $repo->run( qw/diff --cached --name-only/ );
-            my @files = split ' ', $files;
-            unless ( @staged == @files ) {
-                # Numerical equality is Good Enough for now
-                $repo->run( reset => 'HEAD' ); # Unstage the files, it's all gone wrong!
-                return $error->('Some files not staged when "check_all_staged" specified');
-                }
-            }
-        # Files staged and ready to go. Commit time
-        $repo->run( commit => '-m "' . $args{msg} . '"' );
-        }
-    elsif ( $args{all_tracked} ) {
-        ## We want to commit any modified tracked files
-        my @status = $repo->run( status => '-s' );
-        unless ( grep { $_ !~ m/^\?\?/ } @status ) {
-            return $error->('No modified files to commit');
-            }
-        $repo->run( commit => '-a -m "' . $args{msg} . '"' );
-        }
-    elsif ( $args{all_dirty} ) {
-        ## We want to commit all files in their current state
-        my @status = $repo->run( status => '-s' );
-        unless ( @status ) {
-            return $error->('No modified files to commit');
-            }
-        eval { $repo->run( add => $files ); };
-        $repo->run( commit => '-m "' . $args{msg} . '"' );
-        }
-
+    # Looks like we're good to go. Let's commit!
+    my $files = $self->_commit( $args );
     # We've got a new commit. Do we need to worry about a remote?
-    if ( my $remote = $args{use_remote} ) {
-        # Yup, we do. So, get any updates
-        $repo->run( fetch => $remote );
-
-        # Find out if our committed files have been modified on the remote
-        my $branch = $repo->run( 'rev-parse --abbrev-ref HEAD' );
-        my @remote_files = $repo->run( qq#diff $remote/$branch HEAD^ --name-only# );
-        # ^ checks the current head of our branch against the commit BEFORE
-        # the one we just added - we know that the current HEAD has changed
-        # the files we just committed!
-        my %remotes = map { $_ => 1 } @remote_files;
-        if ( grep { $remotes{$_} } @{ split ' ', $files } ) {
-            return $error->('Commit cannot be pushed due to possible conflicts');
-            }
-
-        # Looks like we should be good to go. Push time?
-        # No, we want to 'pull' first, of course. Except ideally, without going back
-        # to the remote, so let's fake it with the fetch we just did
-        $remote->run( "merge FETCH_HEAD" );
-        # Now that we're effectively 'pull'ed up to date, push
-        # Hopefully nobody's had time to push anything else in the tiny window
-        my $push = $remote->run( "push $remote" );
-
-        # Should be ok, but let's make sure
-        if ( $push =~ m#\[rejected\]# ) {
-            return $error->( 'Could not push commit, git returned: ' . $push );
-            }
-        }
+    my $do_remote = $self->_handle_remote( $args, $repo ) if $args->{use_remote};
+    return $error->( $args, $args->{error} ) if $do_remote;;
 
     # Looks like we made it! Run the success sub if appropriate
-    $args{success}->( \%args ) if $args{success};
+    $args->{success}->( $args ) if $args->{success};
 
     return 0;
     }
 
-sub filenames {
+sub _filenames {
     my ( $self, $args ) = @_;
 
     if ( ref $args->{files} eq 'ARRAY' ) {
@@ -121,7 +60,100 @@ sub filenames {
         }
     }
 
+sub _get_repo {
+    my ($self, $args) = @_;
+    $args->{git_dir} ||= './';
+    eval { $repo = Git::Repository->new( git_dir => $args->{git_dir} ); };
+    unless ($@) {
+        return $repo;
+        }
+    $args->{error} = "Unable to initialise git directory:\n" . $@;
+    return;
+    }
+
+sub _commit {
+    my ($self, $args) = @_;
+
+    my $files; # To record what files we intend to commit
+
+    if ( $args->{files} ) {
+        ## We have a list of specified files to commit
+        $files = $self->_filenames( $args );
+        eval { $repo->run( add => $files ); };
+        ## Do we need to make sure all the files had changes to stage?
+        if ( $args->{check_all_staged} ) {
+            my @staged = $repo->run( qw/diff --cached --name-only/ );
+            my @files = split ' ', $files;
+            unless ( @staged == @files ) {
+                # Numerical equality is Good Enough for now
+                $repo->run( reset => 'HEAD' ); # Unstage the files, it's all gone wrong!
+                $args->{error} = 'Some files not staged when "check_all_staged" specified';
+                return;
+                }
+            }
+        # Files staged and ready to go. Commit time
+        $repo->run( commit => '-m "' . $args->{msg} . '"' );
+        }
+    elsif ( $args->{all_tracked} ) {
+        ## We want to commit any modified tracked files
+        my @status = $repo->run( status => '-s' );
+        my @staged;
+        unless ( @staged = grep { $_ !~ m/^\?\?/ } @status ) {
+            $args->{error} = 'No modified files to commit';
+            return;
+            }
+        $files = join ' ', map { $_ =~ s/^\s*\S+\s+(\S+)/$1/ } @staged;
+        $repo->run( commit => '-a -m "' . $args->{msg} . '"' );
+        }
+    elsif ( $args->{all_dirty} ) {
+        ## We want to commit all files in their current state
+        my @status = $repo->run( status => '-s' );
+        unless ( @status ) {
+            $args->{error} = 'No modified files to commit';
+            return;
+            }
+        $files = join ' ', map { $_ =~ s/^\s*\S+\s+(\S+)/$1/ } @status;
+        eval { $repo->run( add => $files ); };
+        $repo->run( commit => '-m "' . $args->{msg} . '"' );
+        }
+
+    return $files;
+    }
+
+sub _handle_remote {
+    my ($self, $args, $repo) = @_;
+    $repo->run( fetch => $remote );
+
+    # Find out if our committed files have been modified on the remote
+    my $branch = $repo->run( 'rev-parse --abbrev-ref HEAD' );
+    my @remote_files = $repo->run( qq#diff $remote/$branch HEAD^ --name-only# );
+    # ^ checks the current head of our branch against the commit BEFORE
+    # the one we just added - we know that the current HEAD has changed
+    # the files we just committed!
+    my %remotes = map { $_ => 1 } @remote_files;
+    if ( grep { $remotes{$_} } @{ split ' ', $files } ) {
+        $args->{error} = 'Commit cannot be pushed due to possible conflicts';
+        return 1;
+        }
+
+    # Looks like we should be good to go. Push time?
+    # No, we want to 'pull' first, of course. Except ideally, without going back
+    # to the remote, so let's fake it with the fetch we just did
+    $remote->run( "merge FETCH_HEAD" );
+    # Now that we're effectively 'pull'ed up to date, push
+    # Hopefully nobody's had time to push anything else in the tiny window
+    my $push = $remote->run( "push $remote" );
+
+    # Should be ok, but let's make sure
+    if ( $push =~ m#\[rejected\]# ) {
+        $args->{error} = 'Could not push commit, git returned: ' . $push;
+        return 1;
+        }
+    return 0;
+    }
+
 1;
+
 __END__
 
 =encoding utf-8
@@ -133,7 +165,7 @@ Git::Archive - For automated git commits
 =head1 SYNOPSIS
 
   use Git::Archive;
-  Git::Archive->commit({ msg => "Committing files", files => (qw/foo bar/) });
+  Git::Archive->commit({ msg => "Committing files", files => [qw/foo bar/] });
 
 =head1 DESCRIPTION
 
@@ -148,7 +180,8 @@ Commit message. This one is mandatory.
 
 =head3 files
 
-List of filenames to commit. Necessary unless you specify all_tracked or all_dirty
+List of filenames to commit. Necessary unless you specify all_tracked or all_dirty.
+Can be a string of space-separated files, or an arrayref of files
 
 =head3 error
 
